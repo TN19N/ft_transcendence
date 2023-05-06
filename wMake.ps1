06f7499fd924f3dbb83f8df55c47f928fe1df6b2
mboukhal <lios80466@gmail.com>
2023-05-06 13:10:49 +0100

    windows make

---
+++ wMake.ps1
@@ -0,0 +1,34 @@
+
+function _build {
+    docker-compose --env-file .\.env build --no-cache
+}
+
+function _start {
+    docker-compose --env-file .\.env up
+}
+
+function _stop {
+    docker-compose down 2> $null
+}
+
+function _clean {
+    _stop
+    docker-compose rm -sf 2> $null
+}
+
+function _fclean {
+    _clean
+    docker system prune -af 2> $null
+    docker volume rm $(docker volume ls -q) 2> $null
+}
+
+Switch ( $args[0] ) {
+
+    "stop" { _stop }
+    "clean" { _clean }
+    "fclean" { _clean; _fclean }
+    "build" { _build }
+    "start" { _build; _start }
+    Default { Write-Host "Useg: $($MyInvocation.MyCommand.Name) [ start | stop | clean | fclean ]" }
+
+}
