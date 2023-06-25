deb20a98a46621cdbf2b9afe058e1e668e30f15e
Mustapha-Annouaoui <MostafaAnawawi@gmail.com>
2023-06-25 18:31:36 +0100

    ..

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-  
+
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true
     }));
